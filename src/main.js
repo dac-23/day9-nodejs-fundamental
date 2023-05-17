007dabd1626ee7b93f2ba728aa03167118ee9378
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-17 10:46:19 +0530

    module system in nodejs.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-// import pattern from "./pattern.js";
-import xpattern from "./pattern.js";
-import { starPattern } from "./pattern.js";
+import pattern from "./pattern.js";
+import { addition } from "./calc.js";
 
 function main() {
   console.log("Hello World");
-  console.log("Progresss and Changesss");
 
-  xpattern();
+  // calling pattern module
+  pattern();
+
+  // calling calc module
+  let sum = addition(10, 20);
+  console.log("SUM=", sum);
 }
 
 main();
