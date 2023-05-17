5f4c3af4a633e2d649b0938cc002a22b3e5b031f
SANTOSH MONDAL <santoshmondal@users.noreply.github.com>
2023-05-17 10:38:42 +0530

    default.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-import pattern from "./pattern.js";
+// import pattern from "./pattern.js";
+import xpattern from "./pattern.js";
 import { starPattern } from "./pattern.js";
 
 function main() {
   console.log("Hello World");
   console.log("Progresss and Changesss");
 
-  pattern();
+  xpattern();
 }
 
 main();
